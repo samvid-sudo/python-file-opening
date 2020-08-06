40c1bc009b97022e7e00ac80bca3e01be872a52c
samvid-sudo <69294224+samvid-sudo@users.noreply.github.com>
2020-08-06 13:43:07 +0530

    Update openfile.py

--- openfile.py
+++ openfile.py
@@ -1,9 +1,9 @@
-file = open('demo.txt')
-read = file.readlines()
-print(read)
+file = open('demo.txt') ## opens demo.txt file
+read = file.readlines() ## this command read the lines of demo.txt and stores in "read" variable
+print(read)  ## prints the lines of demo.txt
 print("\n As you all can see when we type some stuff in text editor the enter is considered as \\n ")
 print("\n It adds new line to a program : ")
 print("\n")
 
-for r in read :
+for r in read :  ## loop for printing the lines in sequence.
     print(r)
